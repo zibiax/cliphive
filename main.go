60520383d257862aba4a86dfd6ba9228d124aaf2
Martin <martin.evenbom@gmail.com>
2024-08-31 22:07:52 +0200

    testing net/http

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "net/http"
+    // "fmt"
 )
 
+//Home handler function
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello from cliphive"))
+}
+
+
 func main() {
-    fmt.Println("Hello, world")
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
+
+
+    log.Println("Starting server on port: 4000")
+
+    // Error handling
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
