29b58a9be4d99d78420e96bb0df8ba1d03afcf2b
Martin <martin.evenbom@gmail.com>
2024-09-06 17:23:25 +0200

    small progress

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,6 +17,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
     files := []string{
         "./ui/html/pages/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
         "./ui/html/pages/home.tmpl",
     }
 
