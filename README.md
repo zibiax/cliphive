82c306f7316c2030427542c81c83872917e5a3d2
Martin <martin.evenbom@gmail.com>
2024-09-09 23:18:36 +0200

    handling unallowed access to backend files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cliphive
 
 Go project to do something similar to pastebin, then we'll see were it goes.
+
+Using mostly core modules.
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "log"
     "net/http"
+    "strings"
 )
 
 
@@ -10,12 +11,14 @@ import (
 func main() {
     mux := http.NewServeMux()
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 
     mux.HandleFunc("/", home)
     mux.HandleFunc("/clip/create", cliphiveCreate)
     mux.HandleFunc("/clip/view", cliphiveView)
+
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 
     log.Println("Starting server on port: 4000")
@@ -24,4 +27,14 @@ func main() {
     // Error handling
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
+}
+
+func neuter(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        if strings.HasSuffix(r.URL.Path, "/") {
+            http.NotFound(w, r)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
 }
