16e89ddb783079805f42e26eefc98aee796fb898
Martin <martin.evenbom@gmail.com>
2024-09-12 16:52:27 +0200

    updated handlers with new logging funcitons

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,7 +10,7 @@ import (
 //Home handler function
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -23,15 +23,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        app.errorLog.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+        app.serverError(w, err)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        app.errorLog.Println(err.Error())
-        http.Error(w, "Internal Server Errpr", 500)
+        app.serverError(w, err)
     }
 
 }
@@ -40,11 +38,11 @@ func (app *application) cliphiveCreate(w http.ResponseWriter, r *http.Request) {
 
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Method Now Allowed", http.StatusMethodNotAllowed)
+        app.clientError(w, http.StatusMethodNotAllowed)
         return
     }
 
-    w.Write([]byte("Create a clip"))
+    w.Write([]byte("Create a clip..."))
 }
 
 func (app *application) cliphiveView(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +51,7 @@ func (app *application) cliphiveView(w http.ResponseWriter, r *http.Request) {
     
     
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
     fmt.Fprintf(w, "Display a specific clip with ID %d...", id)
