1fd1bf5d9ef601170bcb153ecb88ace20049d73a
Martin <martin.evenbom@gmail.com>
2024-09-16 21:08:39 +0200

    small error with static files routing in previous commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,7 +8,7 @@ func (app *application) routes() *http.ServeMux {
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static", http.StripPrefix("/static", fileServer))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
     mux.HandleFunc("/", app.home)
     mux.HandleFunc("/clip/view", app.cliphiveView)
