087bbe11daf2b5b452697e0f7bd9b61597e64482
Martin <martin.evenbom@gmail.com>
2024-09-27 23:54:20 +0200

    some logic for inserting into sql database

--- internal/models/clips.go
+++ internal/models/clips.go
@@ -18,7 +18,18 @@ type ClipsModel struct {
 }
 
 func (m *ClipsModel) Insert(title string, content string, expires int) (int, error) {
-    return 0, nil
+    stmt := `INSERT INTO clips (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(),
+        DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, nil
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, nil
+    }
 }
 
 func(m *ClipsModel) Get(id int) (*Clips, error) {
