ae419d21e76d9104564d898b44681deec8c8709c
Martin <martin.evenbom@gmail.com>
2024-10-06 23:48:23 +0200

    piping function in templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,6 +4,7 @@ import (
     "github.com/zibiax/cliphive/internal/models"
     "html/template"
     "path/filepath"
+    "time"
 )
 
 type templateData struct {
@@ -11,6 +12,12 @@ type templateData struct {
     Clip *models.Clip
     Clips []*models.Clip
 }
+
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{"humanDate": humanDate,}
 
 func newTemplateCache() (map[string]*template.Template, error) {
     cache := map[string]*template.Template{}
@@ -23,7 +30,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
     for _, page := range pages {
         name := filepath.Base(page)
 
-        ts, err := template.ParseFiles("./ui/html/pages/base.tmpl")
+        ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/pages/base.tmpl")
         if err != nil {
             return nil, err
         }
