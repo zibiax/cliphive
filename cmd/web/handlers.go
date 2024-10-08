181a6f65e44b6f70747d2890cb92ffa2475b3b04
Martin <martin.evenbom@gmail.com>
2024-10-09 00:56:20 +0200

    adding template for creating clip and handling for creating clip

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -25,27 +25,31 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
     app.render(w, http.StatusOK, "home.tmpl", data)
 }
 func (app *application) cliphiveCreate(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display the form for creating a new clip"))
+    data := app.newTemplateData(r)
+    app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) cliphiveCreatePost(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+    title := r.PostForm.Get("title")
+    content := r.PostForm.Get("content")
 
-
-
-    title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-    expires := 7
-
+    expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
     id, err := app.clip.Insert(title, content, expires)
     if err != nil {
         app.serverError(w, err)
-        return
     }
 
     http.Redirect(w, r, fmt.Sprintf("/clip/view/%d", id), http.StatusSeeOther)
-
 }
-
 
 func (app *application) cliphiveView(w http.ResponseWriter, r *http.Request) {
     params := httprouter.ParamsFromContext(r.Context())
